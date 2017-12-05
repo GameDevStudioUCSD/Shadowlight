650540c48c7267afa78a6c87b185e332eef52270
ShabbyOWEN <owenchouerzi@gmail.com>
2017-12-04 20:58:16 -0800

    Add LightDoor and ShadowDoor prefab and the LevelSwitchController

---
+++ Assets/Scripts/LevelSwitchController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitchController : MonoBehaviour {
+
+
+  public string nextScene;            // Takes in the name of the scene of next level
+  private Vector3 lightDoorPos;
+  private Vector3 shadowDoorPos;
+  private GameObject lightPlayer;
+  private GameObject shadowPlayer;
+	
+  // Use this for initialization
+	void Start () {
+
+    // Get the position of door objects
+    GameObject lightDoor = GameObject.FindWithTag ("LightDoor");
+    GameObject shadowDoor = GameObject.FindWithTag ("ShadowDoor");
+    lightDoorPos = lightDoor.transform.position;
+    shadowDoorPos = shadowDoor.transform.position;
+
+    lightPlayer = GameObject.FindWithTag ("LightPlayer");
+    shadowPlayer = GameObject.FindWithTag ("ShadowPlayer");
+	}
+	
+	void LateUpdate () {
+
+    // Repeatedly check if player object's rectangle contains door position
+    if (lightPlayer.GetComponent<Collider2D>().bounds.Contains(lightDoorPos)
+      && shadowPlayer.GetComponent<Collider2D>().bounds.Contains(shadowDoorPos) ) {
+        SceneManager.LoadScene(nextScene);
+    }
+
+	}
+}
