f71d2db5b599213ab3f5defc5fb81e78e0d0d114
ShabbyOWEN <owenchouerzi@gmail.com>
2017-12-10 20:28:40 -0800

    Fix styles and make player GameObject public. Scene object stay tuned.

--- Assets/Scripts/LevelSwitchController.cs
+++ Assets/Scripts/LevelSwitchController.cs
@@ -7,31 +7,29 @@ public class LevelSwitchController : MonoBehaviour {
 
 
   public string nextScene;            // Takes in the name of the scene of next level
+  public GameObject lightPlayer;
+  public GameObject shadowPlayer;
+
   private Vector3 lightDoorPos;
   private Vector3 shadowDoorPos;
-  private GameObject lightPlayer;
-  private GameObject shadowPlayer;
-	
+
   // Use this for initialization
-	void Start () {
+  void Start() {
 
     // Get the position of door objects
-    GameObject lightDoor = GameObject.FindWithTag ("LightDoor");
-    GameObject shadowDoor = GameObject.FindWithTag ("ShadowDoor");
+    GameObject lightDoor = GameObject.FindWithTag("LightDoor");
+    GameObject shadowDoor = GameObject.FindWithTag("ShadowDoor");
     lightDoorPos = lightDoor.transform.position;
     shadowDoorPos = shadowDoor.transform.position;
-
-    lightPlayer = GameObject.FindWithTag ("LightPlayer");
-    shadowPlayer = GameObject.FindWithTag ("ShadowPlayer");
-	}
-	
-	void LateUpdate () {
+  }
+  
+  void LateUpdate() {
 
     // Repeatedly check if player object's rectangle contains door position
     if (lightPlayer.GetComponent<Collider2D>().bounds.Contains(lightDoorPos)
       && shadowPlayer.GetComponent<Collider2D>().bounds.Contains(shadowDoorPos) ) {
-        SceneManager.LoadScene(nextScene);
+      SceneManager.LoadScene(nextScene);
     }
 
-	}
+  }
 }
