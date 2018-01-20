8a202432f3ba36438d219f061395dc5d360e2f44
delaneycarmen <delaneycarmen@gmail.com>
2018-01-20 14:28:27 -0800

    Add pressure plate

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+	public bool pressurePlateOn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		pressurePlateOn = true;
+		//some kind of animation ?
+	}
+
+	void OnTriggerExit(Collider other) {
+		pressurePlateOn = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
