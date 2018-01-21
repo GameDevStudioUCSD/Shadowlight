2072375639d9c6ef8e199ce5e5b9f4a403cd1cd0
delaneycarmen <delaneycarmen@gmail.com>
2018-01-20 16:00:08 -0800

    Add button prefab

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	//similar to pressure plate ?
+	//but pressure plate needs continuous contact - has trigger exit function
+
+	public bool pushed = false;
+
+	//TODO 2018-01-20 connect this to circuit ? just a trigger for now
+	    void OnTriggerEnter(Collider other) {
+			//some animation 
+		    pushed = true;
+		}
+		
+}
