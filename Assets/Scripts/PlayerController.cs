5709aa2d7e8f4e09cddf0e061a96275fb1db36fe
eminnguyen <emilyhuongnguyen@gmail.com>
2018-02-01 18:32:20 -0800

    Add jumping character code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,18 @@ public class PlayerController : MonoBehaviour {
   // How fast should the character move horizontally.
   public float horizontalSpeed = 1.0f;
 
+  // The strength of the character's jump
+  public float jumpForce = 5f;
+
+  // How rapidly the character should fall
+  public float fallMulti = 2f;
+
+  // How varied the character's jumps could be
+  public float jumpMulti = 2f;
+
+  // The layers that make up the ground (currently set to Default)
+  public LayerMask groundLayer;
+
   private Rigidbody2D rb2d = null;
 
   private void Start() {
@@ -29,6 +41,7 @@ public class PlayerController : MonoBehaviour {
     // Please change the values in the editor.
     Debug.Assert(inputHorizontal != "", "PlayerController: Horizontal input is empty.", this);
     Debug.Assert(inputJump != "", "PlayerController: Jump input is empty.", this);
+
   }
 
   private void Update() {
@@ -39,11 +52,50 @@ public class PlayerController : MonoBehaviour {
     // TODO 2018-01-20: Maybe we can do gradual acceleration movement.
     tmpVelocity.x = Input.GetAxis(inputHorizontal) * horizontalSpeed;
 
-    if (Input.GetButton(inputJump)) {
-      // TODO 2018-01-20: Jump!
-      Debug.Log("Jump!", this);
+    // Jump Movement
+    // TODO 2018-01-29: Add friction to jump
+    // Once player jumps forward, they should not be allowed to move backwards freely in air
+    if (Input.GetButtonDown(inputJump)) {
+      tmpVelocity.y = Jump();
+    }
+
+    // Character falls faster than it jump
+    if (tmpVelocity.y < 0) {
+      tmpVelocity.y += Physics2D.gravity.y * (fallMulti) * Time.deltaTime;
+    }
+    // Character's jump is based on how long the jump button is held down for
+    else if (tmpVelocity.y > 0 && !Input.GetButton(inputJump)) {
+      tmpVelocity.y += Physics2D.gravity.y * (jumpMulti) * Time.deltaTime;
     }
 
     rb2d.velocity = tmpVelocity;
+  }
+
+  /** Checks if there is ground underneath the object*/
+  bool IsGrounded() {
+
+    // Casts a ray from the center of the object downward to check for ground
+    Vector2 position = transform.position;
+    Vector2 direction = Vector2.down;
+    // The ray's length is slightly longer than half the size of the object
+    float distance = (GetComponent<BoxCollider2D>().bounds.size.y / 2) + 0.02f;
+
+    // TODO 2018-02-01: Make Raycast work with sloped surfaces
+    RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+    Debug.DrawRay(position, direction, Color.blue);
+    if (hit.collider != null) {
+      return true;
+    }
+    return false;
+  }
+
+  /** Handles the character jumping (Checks if grounded)*/
+  float Jump() {
+
+    if (!IsGrounded()) {
+      return 0;
+    }
+    return jumpForce;
+    }
   }
 }
