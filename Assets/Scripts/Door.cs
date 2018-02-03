5d986c6d6979ab94941716d6ce90a30f538972f1
ejoun6718 <hjoun@ucsd.edu>
2018-02-02 18:22:27 -0800

    Set idle animation as default and add door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.Assertions;
+
+/**
+ * Class for a door that can open and close.
+ */
+[RequireComponent(typeof(Animator))]
+public class Door : MonoBehaviour
+{
+    public UnityEvent onOpen;
+    public UnityEvent onClose;
+    private Animator animator = null;
+    private bool open = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        // Should not be null because of the RequireComponent attribute.
+        Assert.IsNotNull(animator, name + " requires an Animator component.");
+    }
+
+    public void Open()
+    {
+        open = true;
+        onOpen.Invoke();
+    }
+
+    public void Close()
+    {
+        open = false;
+        onClose.Invoke();
+    }
+
+    public void Switch()
+    {
+        if (open)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+}
