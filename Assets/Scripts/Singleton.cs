4b91a1eb7ab3900ce5bd6db660bbcae478627d9a
ejoun6718 <hjoun@ucsd.edu>
2018-02-22 22:38:32 -0800

    Add Singleton script that allows music object to play over all scenes

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+    private static Singleton instance = null;
+
+	// Use this for initialization
+	void Awake () {
+        if(instance != null && instance != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+	}
+}
