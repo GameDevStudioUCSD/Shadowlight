5c1edec2bc4effe98e5095cd156f4fa37d4c9124
Christina Mak <ChristinaMak@users.noreply.github.com>
2018-03-03 16:04:50 -0800

    Added castle level with moving walls, levers, and pressure plates

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -14,6 +14,7 @@ public class Lever : MonoBehaviour {
     public UnityEvent leftMode = null;
     public UnityEvent rightMode = null;
     private Animator animator = null;
+    public bool startNoAction = false;
     private bool left = true;
     private bool inRange = false;
 
@@ -26,14 +27,21 @@ public class Lever : MonoBehaviour {
 
     private void OnEnable()
     {
-        // Invoke the event associated with the default state of the lever
-        if (left)
+        if (!startNoAction)
         {
-            leftMode.Invoke();
+            // Invoke the event associated with the default state of the lever
+            if (left)
+            {
+                leftMode.Invoke();
+            }
+            else
+            {
+                rightMode.Invoke();
+            }
         }
         else
         {
-            rightMode.Invoke();
+            startNoAction = !startNoAction;
         }
     }
 
@@ -56,7 +64,6 @@ public class Lever : MonoBehaviour {
         // If toggled left, switch to right
         if(left)
         {
-
             rightMode.Invoke();
             animator.Play("ToggleRight");
         }
