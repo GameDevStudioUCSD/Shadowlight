092b9e9dfc779557d1b244c7fe78c3fd0fca8ccf
ejoun6718 <hjoun@ucsd.edu>
2018-03-10 16:19:51 -0800

    Add interactable object indicator

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,66 +4,9 @@ using UnityEngine;
 using UnityEngine.Events;
 
 /**
- * Script to attach to all interactable objects. Detects when Light or Shadow
- * are within range to interact with the object.
+ * Script to attach to all interactable objects. Able to assign and invoke
+ * the interact event.
  */
 public class Interactable : MonoBehaviour {
-    private string inRange = "";    // Marks which character is in range
     public UnityEvent interact = null;
-    public UnityEvent lightPlayerEnter = null;
-    public UnityEvent shadowPlayerEnter = null;
-    public UnityEvent lightPlayerExit = null;
-    public UnityEvent shadowPlayerExit = null;
-
-	// Update is called once per frame
-	void Update () {
-        if (inRange == "LightPlayer" && Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            interact.Invoke();
-        }
-        if (inRange == "ShadowPlayer" && Input.GetKeyDown(KeyCode.S))
-        {
-            interact.Invoke();
-        }
-	}
-
-    // Because OnTriggerStay2D was only being called when collider was moving
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        print("onTriggerEnter called");
-        // Check that only a player object can interact with the lever
-        if (other.GetComponent<PlayerController>())
-        {
-            // Distinguish between Light and Shadow using tags
-            if (other.tag == "LightPlayer")
-            {
-                inRange = "LightPlayer";
-                lightPlayerEnter.Invoke();
-            }
-            else if (other.tag == "ShadowPlayer")
-            {
-                inRange = "ShadowPlayer";
-                shadowPlayerEnter.Invoke();
-            }
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.GetComponent<PlayerController>())
-        {
-            if (other.tag == "LightPlayer")
-            {
-                inRange = "LightPlayer";
-                lightPlayerExit.Invoke();
-            }
-            else if (other.tag == "ShadowPlayer")
-            {
-                inRange = "ShadowPlayer";
-                shadowPlayerExit.Invoke();
-            }
-
-            inRange = "";
-        }
-    }
 }
