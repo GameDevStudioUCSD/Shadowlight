82586bad62a7fa624a864f4016dd5532028361fa
Christina Mak <ChristinaMak@users.noreply.github.com>
2018-03-10 20:39:48 -0800

    Plant that grows when light touches it

---
+++ Assets/Scripts/GrowingPlant.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingPlant : MonoBehaviour {
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Grow()
+    {
+        anim.SetTrigger("LightTouching");
+    }
+
+    public void Shrink()
+    {
+
+    }
+}
