8fdd5975ef79c49a07872dc49ce5da8bc2cf8977
ejoun6718 <hjoun@ucsd.edu>
2018-03-24 22:06:54 -0700

    Added new level in CastleLevel scene

---
+++ Assets/Scripts/Rideable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rideable : MonoBehaviour {
+
+    private void Update()
+    {
+        foreach (Transform child in transform) {
+            child.Translate(new Vector3(transform.position.x, child.transform.position.y, child.transform.position.z));
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        print("called");
+        if (other.gameObject.GetComponent<Rigidbody2D>())
+        {
+                other.transform.parent = transform;
+            //other.transform.GetComponent<Rigidbody2D>();
+                print("called");
+        }
+    
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.transform.parent = null;
+    }
+}
