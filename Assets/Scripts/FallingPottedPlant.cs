5847ac5b0fb52205ffba0ce51d6ee69932bb3532
alichee <alissaren98@gmail.com>
2018-04-28 16:08:09 -0700

    Garden level

--- Assets/Scripts/FallingPottedPlant.cs
+++ Assets/Scripts/FallingPottedPlant.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(DistanceJoint2D))]
+[RequireComponent(typeof(LineRenderer))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class FallingPottedPlant : MonoBehaviour {
 
 	private Animator anim = null;
 	private DistanceJoint2D joint = null;
+	private LineRenderer lineRenderer = null;
 	private Rigidbody2D rb2d = null;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		joint = GetComponent<DistanceJoint2D> ();
+		lineRenderer = GetComponent<LineRenderer> ();
 		rb2d = GetComponent<Rigidbody2D> ();
-
-		GameObject line = transform.GetChild (0).gameObject;
-		Vector3 lineScale = line.transform.localScale;
-		lineScale.y = (joint.anchor - joint.connectedAnchor).magnitude;
-		line.transform.localScale = lineScale;
-		line.transform.position += new Vector3(0.0f, lineScale.y / 2.0f * 0.04f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		anim.SetFloat ("yVelocity", rb2d.velocity.y);
+		lineRenderer.SetPosition (0, transform.position + (Vector3)joint.anchor);
+		lineRenderer.SetPosition (1, joint.connectedAnchor);
 	}
 
 	public void Fall() {
 		joint.enabled = false;
+		lineRenderer.enabled = false;
 	}
 }
