bfc92e1b6c28291b82d3cd7e3b4322903341125e
ejoun6718 <hjoun@ucsd.edu>
2018-05-12 15:15:24 -0700

    Created spring prefab and level

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Spring : MonoBehaviour {
+    private Animator animator = null;
 
 	// Use this for initialization
 	void Start () {
-		
+        animator = GetComponent<Animator>();
+        // Should not be null because of the RequireComponent attribute.
+        Assert.IsNotNull(animator, name + " requires an Animator component.");
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        print("boing");
+        //if (other.GetComponent<Rigidbody2D>().velocity.y < 0)
+        //{
+            animator.Play("Boing");
+        //}
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
     }
 }
