e0d98e242963f4cf0ae8e8b2450d1699aa46fd69
ejoun6718 <hjoun@ucsd.edu>
2018-05-12 16:27:03 -0700

    Add prism animation

--- Assets/Scripts/Prism.cs
+++ Assets/Scripts/Prism.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 /**
  * Class for the prism object, which switches the current positions of Light
  * and Shadow when it is interacted by with either character.
  */
+[RequireComponent(typeof(SpriteRenderer))]
 public class Prism : MonoBehaviour {
+    private Animator animator = null;
+    private SpriteRenderer flashSprite = null;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+        // Should not be null because of the RequireComponent attribute.
+        Assert.IsNotNull(animator, name + " requires an Animator component.");
+
+        GameObject flash = flash = transform.Find("Flash").gameObject;
+        flashSprite = flash.GetComponent<SpriteRenderer>();
+    }
+
     public void SwitchPositions() {
+        animator.Play("Active");
+        Invoke("MovePlayers", 1.4f);
+
+    }
+
+    void MovePlayers() {
+        Flash();
+
         // Find Light and Shadow in the scene
         Transform lightPlayer = GameObject.Find("LightPlayer").transform;
         Transform shadowPlayer = GameObject.Find("ShadowPlayer").transform;
@@ -20,5 +43,9 @@ public class Prism : MonoBehaviour {
         Vector3 lightPlayerPosition = lightPlayer.position;
         lightPlayer.position = shadowPlayer.position;
         shadowPlayer.position = lightPlayerPosition;
+    }
+
+    void Flash() {
+        
     }
 }
