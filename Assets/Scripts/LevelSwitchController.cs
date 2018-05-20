2be20526cf43704c8c834b248f3927c073c6d95e
Sung Rim Huh <sungrimhuh@gmail.com>
2018-05-19 17:42:40 -0700

    Streamline level switching
    The next level is determined based on Build settings, which you can
    acess by File -> Build Settings...

--- Assets/Scripts/LevelSwitchController.cs
+++ Assets/Scripts/LevelSwitchController.cs
@@ -5,53 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class LevelSwitchController : MonoBehaviour {
 
-
-  public string nextScene; // The name of the next level scene
-  public GameObject lightPlayer;
-  public GameObject shadowPlayer;
-
-  private Bounds lightDoorBounds;
-  private Bounds shadowDoorBounds;
+    private Bounds lightDoorBounds;
+    private Bounds shadowDoorBounds;
     private GameObject lightDoor;
     private GameObject shadowDoor;
 
-  void Start() {
+    void Start() {
 
-    // Get the position of door objects
-    lightDoor = GameObject.FindWithTag("LightDoor");
-    shadowDoor = GameObject.FindWithTag("ShadowDoor");
-    lightDoorBounds = lightDoor.GetComponent<Collider2D>().bounds;
-    shadowDoorBounds = shadowDoor.GetComponent<Collider2D>().bounds;
-  }
+        // Get the position of door objects
+        lightDoor = GameObject.FindWithTag("LightDoor");
+        shadowDoor = GameObject.FindWithTag("ShadowDoor");
+        lightDoorBounds = lightDoor.GetComponent<Collider2D>().bounds;
+        shadowDoorBounds = shadowDoor.GetComponent<Collider2D>().bounds;
+    }
 
-  void LateUpdate() {
-    // This is in late update because the bound checking is not as important as
-    // updating game physics and animation.
+    void LateUpdate() {
+        // This is in late update because the bound checking is not as important as
+        // updating game physics and animation.
 
-    // TODO 2018-01-13: Right now, this is checking almost every frame. It might
-    // be better to split the actual checking of the character inside door and
-    // the switching level into separate component. That way, the component can
-    // check the character bound on OnCollider*2D functions.
+        // TODO 2018-01-13: Right now, this is checking almost every frame. It might
+        // be better to split the actual checking of the character inside door and
+        // the switching level into separate component. That way, the component can
+        // check the character bound on OnCollider*2D functions.
 
-    Bounds lightPlayerBounds = lightPlayer.GetComponent<Collider2D>().bounds;
-    Bounds shadowPlayerBounds = shadowPlayer.GetComponent<Collider2D>().bounds;
+        Bounds lightPlayerBounds = Globals.lightPlayer.GetComponent<Collider2D>().bounds;
+        Bounds shadowPlayerBounds = Globals.shadowPlayer.GetComponent<Collider2D>().bounds;
 
-    // This checks to make sure that the character is completely inside the
-    // door's collider.
-    if (lightDoorBounds.Contains(lightPlayerBounds.min)
-      && lightDoorBounds.Contains(lightPlayerBounds.max)
-      && shadowDoorBounds.Contains(shadowPlayerBounds.min)
-      && shadowDoorBounds.Contains(shadowPlayerBounds.max)) {
+        // This checks to make sure that the character is completely inside the
+        // door's collider.
+        if (lightDoorBounds.Contains(lightPlayerBounds.min)
+            && lightDoorBounds.Contains(lightPlayerBounds.max)
+            && shadowDoorBounds.Contains(shadowPlayerBounds.min)
+            && shadowDoorBounds.Contains(shadowPlayerBounds.max)) {
             lightDoor.GetComponent<Animator>().SetTrigger("Open");
             shadowDoor.GetComponent<Animator>().SetTrigger("Open");
             //Globals.lightPlayer.GetComponent<PlayerController>().enabled = false;
             //Globals.shadowPlayer.GetComponent<PlayerController>().enabled = false;
             StartCoroutine("LoadScene", 0.5f);
+        }
     }
-  }
 
     IEnumerator LoadScene(float delay) {
         yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene(nextScene);
+        int buildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (buildIndex < SceneManager.sceneCountInBuildSettings) {
+            SceneManager.LoadScene(buildIndex);
+        }
     }
 }
