06b439002b25e76dcebeaf0af1f42c892d025eac
Sung Rim Huh <sungrimhuh@gmail.com>
2018-06-13 18:05:16 -0700

    Add Theme script along with themes

---
+++ Assets/Scripts/Theme.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Theme.asset")]
+public class Theme : ScriptableObject {
+
+    public Sprite background;
+    public Sprite platform;
+    public Sprite wall;
+}
